dfdd0f81b43d7fb9f16ea034b5287f304938f22f
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-06-23 21:40:30 -0400

    added some comments

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,9 +1,12 @@
 async function loginFormHandler(event) {
   event.preventDefault();
 
+  //   form, get username and password. value and trim
   const userName = document.querySelector("#username-login").value.trim();
   const password = document.querySelector("#password-login").value.trim();
 
+  //   use the username and password of inputs to login!
+  //  this will match the post method in API routes /login
   if (userName && password) {
     const response = await fetch("/api/users/login", {
       method: "post",
@@ -15,6 +18,7 @@ async function loginFormHandler(event) {
     });
 
     if (response.ok) {
+      // send to dashboard where the user can see their posts!
       document.location.replace("/dashboard/");
     } else {
       alert(response.statusTexts);
